4f64146f2da825a7d4ed2dae5f64dab04022d53d
Sławek Kluczyk <slawomir.kluczyk@keyworks.pl>
2015-11-26 17:11:01 +0100

    Added validators and updated devise options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable, :trackable, :rememberable
+  devise :database_authenticatable, :registerable, :trackable, :rememberable
+
+  validates :email, format: { with: Devise::email_regexp }
+  validates :password, presence: true
 end
