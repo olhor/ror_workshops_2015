1833f90d2c143b743681bc7ebf0bca96a23e5328
Sławek Kluczyk <slawomir.kluczyk@keyworks.pl>
2015-11-26 13:28:45 +0100

    Fixed ReportsController failing tests
     - Added missing devise configuration to User model
     - Added missing routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable, :recoverable, :trackable, :rememberable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get :subjects, to: 'reports#subjects'
+
   resources :students do
     get :subjects
   end
